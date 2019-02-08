654b73c2eaddd0ccabb87de03b0814142fcb39f6
LydiaWu <lxw7227@g,rit.edu>
2019-02-08 11:33:46 +0800

    created the file

---
+++ Lydia Wu Hello Git/Lydia Wu Hello Git/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lydia_Wu_Hello_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
